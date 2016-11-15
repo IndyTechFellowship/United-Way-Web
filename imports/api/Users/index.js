a71bdb6494383028c42a1cfe0f73f3a13c845b98
Mike Hockerman <mike@hockerman.com>
2016-11-14 22:24:29 -0500

    Add basic users schema, bootstrap into /imports/startup/server from /server

--- imports/api/Users/index.js
+++ imports/api/Users/index.js
@@ -1,1 +1,30 @@
-asdf
+import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { Meteor } from 'meteor/meteor'
+import { Mongo } from 'meteor/mongo'
+
+const ProfileSchema = new SimpleSchema({
+  avatarUrl: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+})
+
+const UserSchema = new SimpleSchema({
+  profile: {
+    type: ProfileSchema,
+  },
+})
+
+const Users = new Mongo.Collection('users')
+Users.attachSchema(UserSchema)
+
+export { Users }
+
