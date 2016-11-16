d3272e7d393e0683f78b20d83b35865eb9045480
Ben Wencke <benwencke@gmail.com>
2016-11-15 23:03:19 -0500

    Fixed broken import

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import React from 'react'
 import { render } from 'react-dom'
 
-import { Routes } from '/imports/startup/client/Routes'
+import { Routes } from '/imports/startup/client/routes'
 
 Meteor.startup(() => {
   render(<Routes />, document.getElementById('app'));
