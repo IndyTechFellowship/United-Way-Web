858fc550f849f1d8e6356644c9f2b457a19a7bc5
Tylor Garrett <tylorgarrett@gmail.com>
2016-11-20 19:44:44 -0500

    updated gitignore and started toolbar work

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
+import {
+	Toolbar, 
+	ToolbarGroup,
+	FlatButton,
+	AutoComplete} from 'material-ui'
 
 class Navbar extends Component {
 
+	constructor(props) {
+		super(props)
+		this.state = {
+			dataSource: []
+		}
+	}
+
   render() {
-    return <div>Navbar</div>
+    return (
+    	<div>
+    		<Toolbar>
+    			<ToolbarGroup firstChild={true} style={toolbarGroupStyle}>
+    				<img src="auw.png" height="45px" width="45px" />
+    				<FlatButton label="Feed" />
+    				<FlatButton label="About" />
+    				<AutoComplete
+          				hintText="Search"
+          				dataSource={this.state.dataSource}
+          				onUpdateInput={this.handleUpdateInput}
+        			/>
+    				<FlatButton label="Agencies" />
+    				<FlatButton label="People" />
+    			</ToolbarGroup>
+    		</Toolbar>
+    	</div>
+    )
   }
 
+  handleUpdateInput() {
+
+  }
+}
+
+const toolbarGroupStyle = {
+	margin: 'auto',
+    width: '50%'
 }
 
 export default Navbar
--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import App from '/imports/ui/components/App'
+
+injectTapEventPlugin();
 
 class AppContainer extends Component {
 
   render() {
-    return <App children={ this.props.children } />
+  	return (
+  		<MuiThemeProvider>
+  			<App children={ this.props.children } />
+  		</MuiThemeProvider>
+  	)
   }
 
 }
