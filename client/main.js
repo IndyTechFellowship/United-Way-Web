723d109c9fb49c15ff91171fa811b226a1657e8c
Ben Wencke <benwencke@gmail.com>
2016-11-24 10:27:59 -0500

    Added material-ui and page wrapper

--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
 import { Meteor } from 'meteor/meteor'
 import React from 'react'
 import { render } from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import { Routes } from '/imports/startup/client/routes'
+
+injectTapEventPlugin();
 
 Meteor.startup(() => {
   render(<Routes />, document.getElementById('app'));
