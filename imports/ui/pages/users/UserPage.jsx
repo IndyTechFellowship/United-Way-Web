c76509590407835fe8f023e0ff74354cc9c83a3d
Ben Wencke <benwencke@gmail.com>
2016-11-24 10:58:55 -0500

    Added two column layout to user page

--- imports/ui/pages/users/UserPage.jsx
+++ imports/ui/pages/users/UserPage.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 
+const styles = {
+  twoColumnLayout: {
+    display: "flex",
+    alignItems: "stretch",
+    flexWrap: "wrap",
+  },
+  columnOne: {
+    flex: 2,
+    minWidth: 300,
+  },
+  columnTwo: {
+    flex: 3,
+    minWidth: 300,
+  }
+}
+
 class UserPage extends Component {
 
   render() {
-    return <div>User { this.props.params.id }</div>
+    return (
+      <div style={styles.twoColumnLayout}>
+        <div style={styles.columnOne}>
+          User { this.props.params.id } Column 1
+        </div>
+        <div style={styles.columnTwo}>
+          User { this.props.params.id } Column 2
+        </div>
+      </div>
+    )
   }
 
 }
