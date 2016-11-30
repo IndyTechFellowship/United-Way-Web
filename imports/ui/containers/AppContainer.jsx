98daa82f00f19ffe7ea9b1f1a403250a643a588f
Tylor Garrett <tylorgarrett@gmail.com>
2016-11-29 21:43:53 -0500

    updated appcontainer

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from 'react'
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import App from '/imports/ui/components/App'
-
-injectTapEventPlugin();
 
 class AppContainer extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
         <App children={ this.props.children } />
-      </MuiThemeProvider>
     )
   }
 
