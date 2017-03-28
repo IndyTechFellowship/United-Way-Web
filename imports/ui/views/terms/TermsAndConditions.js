6494159052d869818600bf27adc60965dfb33670
Tylor Garrett <tylorgarrett@gmail.com>
2017-03-27 20:43:46 -0400

    making sure terms and conditions are updated

--- imports/ui/views/terms/TermsAndConditions.js
+++ imports/ui/views/terms/TermsAndConditions.js
@@ -5,7 +5,14 @@ export default class TermAndConditions extends Component {
         return (
           <div>
             <h1>Terms and Conditions</h1>
-            <p>Under all terms, you have agreed to the conditions. Thank you</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare eleifend enim. Fusce rutrum vel velit at hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis at nunc quis lectus mollis mattis et eu ex. Nunc quis quam eget mauris consequat mattis quis sit amet ligula. Praesent suscipit justo eu elit pharetra porttitor. Aliquam erat volutpat. Curabitur sed efficitur quam. Aenean a orci ante.</p>
+            <p>Phasellus tincidunt mattis dolor quis molestie. In suscipit accumsan convallis. Integer tristique elit et justo cursus malesuada. In hac habitasse platea dictumst. Donec laoreet justo vitae consectetur molestie. Vestibulum nisl libero, mattis a massa non, tempor iaculis nisl. Integer accumsan, diam ac luctus sodales, purus nulla consectetur erat, quis ullamcorper metus ante eu tellus. Proin porttitor nisl sit amet ipsum pulvinar faucibus. In hac habitasse platea dictumst. Nullam et est in purus tincidunt dapibus vel ut ligula. Vestibulum enim velit, viverra vel aliquam eget, consectetur at velit. Maecenas in porttitor felis. Proin vulputate purus id pretium aliquet. Proin id purus cursus est tincidunt egestas vitae a purus.</p>
+            <h1>Privacy Policy</h1>
+            <h3>Information, Collection, Use and Sharing</h3>
+            <p>We are the sole owners of the information collected on the site. We only have access to and collect information that you voluntarily give us via email or other direct contact from you. We will not sell or rent this information to anyone.
+We will use your information to respond to you regarding the reason you contacted us. We will not share your information with any third-party outside organization, other then as necessary to fulfill your request. Unless you asked us not to, we may contact you in the future to tell you about United Way's work and I work community and opportunities for youth engage and our community Through United Way, or changes to this privacy policy. </p>
+            <h3>Your Access to and Control Over Information</h3>
+            <p>You may opt out of any future contacts from us at any time. You can do the following at any time by contacting us via the email address or phone number given on our website: uwci.org/contact-us</p>
           </div>
         )
     }
