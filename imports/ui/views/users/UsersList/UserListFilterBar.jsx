2eaad080293e3e18a2583aa9e759b28bdcac8f92
Mike Hockerman <mike@hockerman.com>
2017-06-08 22:01:28 -0400

    Add an X button to the search filter bar

--- imports/ui/views/users/UsersList/UserListFilterBar.jsx
+++ imports/ui/views/users/UsersList/UserListFilterBar.jsx
@@ -1,9 +1,35 @@
+import {
+  FlatButton,
+} from 'material-ui'
+import ContentClear from 'material-ui/svg-icons/content/clear';
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 
-const UserListFilterBar = ({ }) => {
+import {
+  setUserSearchResults,
+} from '/imports/ui/state'
+
+const UserListFilterBar = ({ dispatch }) => {
   return (
-    <span>FILTERED!</span>
+    <div>
+      <span>FILTERED!</span>
+      <FlatButton
+        icon={<ContentClear />}
+        onTouchTap={onXClick(dispatch)} />
+    </div>
   )
 }
 
-export default UserListFilterBar;
+const onXClick = (dispatch) => () => {
+  dispatch(setUserSearchResults(null))
+}
+
+UserListFilterBar.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = {
+
+}
+
+export default connect()(UserListFilterBar);
