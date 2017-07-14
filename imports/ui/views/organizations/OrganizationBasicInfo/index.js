23f1926b460870c1779584dfb41f8e442f28fa90
Ben Wencke <benwencke@gmail.com>
2017-07-13 22:05:07 -0400

    Starting

--- imports/ui/views/organizations/OrganizationBasicInfo/index.js
+++ imports/ui/views/organizations/OrganizationBasicInfo/index.js
@@ -1,14 +1,58 @@
+import _ from 'lodash'
 import React, {Component} from 'react'
 
 import { RaisedButton } from 'material-ui'
 
+import EditButton from '/imports/ui/components/EditButton'
 import OrganizationDetails from './OrganizationDetails'
 import Skills from '/imports/ui/components/Skills'
 import AvatarCard from '/imports/ui/components/AvatarCard'
 
 class OrganizationBasicInfo extends Component {
+  constructor(props) {
+    super(props)
+    let tagsArray = this.props.organization.tags
+    this.state = {
+      isEditing: false,
+      name: this.props.organization.name,
+      tagline: this.props.organization.tagline,
+      tags: _.fill(tagsArray, null, tagsArray.length-1, 5)
+    }
+    this.edit = this.edit.bind(this)
+    this.save = this.save.bind(this)
+    this.selectTag = this.selectTag.bind(this)
+  }
+
+  edit() {
+    this.setState({ isEditing: true })
+  }
+
+  save() {
+    this.setState({ isEditing: false })
+    const profile = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      tagline: this.state.tagline,
+      skills: this.state.skills
+    }
+    let newUser = _.merge({}, this.props.user, { profile: profile })
+    Meteor.call('User.update', newUser, (err, resp) => {
+    })
+  }
+
+
+  selectTag(index, selectedIndex) {
+    const newTag= this.props.tags[selectedIndex]
+    let tags = _.cloneDeep(this.state.tags)
+    tags[index] = _.cloneDeep(newTag)
+    this.setState({ tags: tags })
+  }
+
   render() {
-    return (
+    const editing = (
+      <div>hello</div>
+    )
+    const viewing = (
       <div style={styles.twoColumnLayout}>
         <div style={styles.outerContainer}>
           <div style={styles.column}>
@@ -24,6 +68,16 @@ class OrganizationBasicInfo extends Component {
         <div style={styles.buttonContainer}>
           <RaisedButton label="Follow Organization" fullWidth={true} />
         </div>
+      </div>
+    )
+    return (
+      <div>
+        <EditButton
+          isEditing={this.state.isEditing}
+          edit={this.edit}
+          save={this.save}
+        />
+        {this.state.isEditing ? editing : viewing}
       </div>
     )
   }
