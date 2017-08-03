5d2867bdec817f2d9e0f456ede1fa55ce438811e
Mike Hockerman <mike@hockerman.com>
2017-08-02 22:39:14 -0400

    rewrite organizations.update

--- imports/api/Organizations/organizationsMethods.js
+++ imports/api/Organizations/organizationsMethods.js
@@ -1,16 +1,28 @@
+import { isObject, map, omit } from 'lodash'
 import { check, Match } from 'meteor/check'
 import { Meteor } from 'meteor/meteor'
 
 import { Organizations } from './index'
 
 Meteor.methods({
+  
   'Organization.update'(organization) {
-    if (this.connection) {
-      organization.tags = organization.tags.map(i => i._id)
-      const newOrganization = Organizations.update(organization._id, { $set: organization })
-      if (!newOrganization) throw Meteor.Error(401, 'unauthorized')
-      return newOrganization
+    const callingUser = Users.findOne({ _id: this.userId });
+    if (!callingUser) {
+      throw Meteor.Error(401, 'unauthorized')
     }
-    throw Meteor.Error(401, 'unauthorized')
-  }
+    if (organization.tags) {
+      organization.tags = map(organization.tags, t => {
+        if (isObject(t)) return t._id
+        else return t
+      })
+    }
+    Organizations.update({
+      _id: organization._id,
+    }, {
+      $set: omit(organization, [ '_id' ]),
+    })
+    return true
+  },
+
 })
