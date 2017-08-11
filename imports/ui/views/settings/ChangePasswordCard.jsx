947772c41c42f23b2210726a4eb3606c1f09f418
Mike Hockerman <mike@hockerman.com>
2017-08-10 21:00:37 -0400

    Wire up to change password Accounts function

--- imports/ui/views/settings/ChangePasswordCard.jsx
+++ imports/ui/views/settings/ChangePasswordCard.jsx
@@ -1,7 +1,8 @@
-import { Col } from 'jsxstyle'
+import { Col, Row } from 'jsxstyle'
 import { 
   Card,
   CardHeader,
+  CircularProgress,
   RaisedButton,
   TextField,
 } from 'material-ui'
@@ -9,11 +10,31 @@ import Lock from 'material-ui/svg-icons/action/lock';
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import {
+  changePassword,
+} from '/imports/ui/state'
+
 const styles = { 
   body: {
     alignItems: 'flex-end',
     padding: '0px 24px 24px 24px',
-  }
+  },
+  buttonRow: {
+    alignItems: 'center',
+  },
+  leftItem: {
+    paddingRight: '8px',
+  },
+  error: {
+    color: 'red',
+    fontSize: '12px',
+    paddingRight: '8px',
+  },
+  confirm: {
+    color: 'green',
+    fontSize: '12px',
+    paddingRight: '8px',
+  },
 };
 
 class ChangePasswordCard extends Component {
@@ -21,10 +42,36 @@ class ChangePasswordCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      confirm: null,
       current: '',
+      error: null,
+      loading: false,
       new1: '',
       new2: '',
     };
+  }
+
+  changePassword() {
+    const { changePassword } = this.props
+    const { current, new1, new2 } = this.state
+    if (new1 !== new2) {
+      return this.setState({ error: 'Your passwords must match' })
+    }
+    this.setState({ confirm: null, error: null, loading: true })
+    changePassword(current, new1, (err) => {
+      this.setState({ loading: false })
+      if (err) {
+        this.setState({ confirm: null, error: err.reason })
+      } else {
+        this.setState({ 
+          confirm: 'Your password has been reset!', 
+          current: '',
+          error: null,
+          new1: '',
+          new2: '',
+        })
+      }
+    })
   }
 
   render() {
@@ -57,10 +104,16 @@ class ChangePasswordCard extends Component {
             type='password'
             value={this.state.new2}
           />
-          <RaisedButton
-            label='Submit'
-            primary={true}
-          />
+          <Row style={styles.buttonRow}>
+            {this.state.loading && <CircularProgress size={30} style={styles.leftItem} />}
+            {this.state.error && <span style={styles.error}>{this.state.error}</span>}
+            {this.state.confirm && <span style={styles.confirm}>{this.state.confirm}</span>}
+            <RaisedButton
+              label='Submit'
+              onTouchTap={this.changePassword.bind(this)}
+              primary={true}
+            />
+          </Row>
         </Col>
       </Card>
     );
@@ -68,4 +121,8 @@ class ChangePasswordCard extends Component {
 
 }
 
-export default ChangePasswordCard;
+const mapDispatchToProps = (dispatch) => ({
+  changePassword: (oldp, newp, done) => dispatch(changePassword(oldp, newp, done)),
+})
+
+export default connect(null, mapDispatchToProps)(ChangePasswordCard);
