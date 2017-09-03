b6da6e1b669d8a30fe826aec9d4af7200ba51bd8
Ben Wencke <benwencke@gmail.com>
2017-09-03 16:11:43 -0400

    working on home page

--- imports/ui/views/home/HomePage.jsx
+++ imports/ui/views/home/HomePage.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 
+import Carousel from '/imports/ui/components/Carousel'
 import Title from '/imports/ui/components/Title'
 import OrganizationCarousel from '/imports/ui/views/home/OrganizationCarousel'
 import PositionCarousel from '/imports/ui/views/home/PositionCarousel'
+import Slider from 'react-slick'
 import UserCarousel from '/imports/ui/views/home/UserCarousel'
 
 class HomePage extends Component {
 
   render() {
+    const settings = {
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 500
+    }
+
     return (
       <div>
-        <div>
-          <img src="organizations-banner.png" style={styles.img} />
+        <div style={styles.images}>
+          <Slider {...settings} style={styles.slider}>
+            <div><img src="volunteers-banner.png"  style={styles.img} /></div>
+            <div><img src="positions-banner.png" style={styles.img} /></div>
+            <div><img src="organizations-banner.png" style={styles.img} /></div>
+          </Slider>
         </div>
 
         <div style={styles.opportunityLabel}>Positions</div>
@@ -30,13 +43,7 @@ class HomePage extends Component {
 
 const styles = {
   images: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  img: {
-    margin: '4px',
+    width: '100%'
   },
   title: {
     marginTop: '32px',
@@ -46,6 +53,12 @@ const styles = {
     fontSize: '24px',
     fontWeight: 'bold',
     margin: '12px'
+  },
+  slider: {
+    width: '100%'
+  },
+  img: {
+    width: '100%'
   }
 }
 
