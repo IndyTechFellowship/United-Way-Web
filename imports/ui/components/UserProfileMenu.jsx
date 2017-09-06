cdd1da9800f93553985d148c18864c6a4fabf043
Mike Hockerman <mike@hockerman.com>
2017-09-05 22:06:21 -0400

    Clean up the nav bar dropdown.

--- imports/ui/components/UserProfileMenu.jsx
+++ imports/ui/components/UserProfileMenu.jsx
@@ -1,78 +1,84 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { browserHistory } from 'react-router'
+import { get } from 'lodash';
+import { Col, Row } from 'jsxstyle';
 import {
   Divider,
   FlatButton,
   IconButton,
   IconMenu,
   MenuItem,
-} from 'material-ui'
-import { lightBlue800 } from 'material-ui/styles/colors'
+} from 'material-ui';
 import Person from 'material-ui/svg-icons/action/account-circle'
+import { lightBlue800 } from 'material-ui/styles/colors';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 
+import { Colors } from '/imports/ui/styles';
 import {
-  signoutUser
+  signoutUser,
 } from '/imports/ui/state'
 
 class UserProfileMenu extends Component {
+  
   constructor(props) {
     super(props)
     this.state = {
       userInitials: ""
     }
-    this.getUserInitials = this.getUserInitials.bind(this)
-    this.getPlaceholder = this.getPlaceholder.bind(this)
-    this.goToUserProfile = this.goToUserProfile.bind(this)
+    this.getAvatar = this.getAvatar.bind(this);
+    this.goToUserProfile = this.goToUserProfile.bind(this);
   }
 
   render() {
-    let profile = Meteor.user().profile
-    let name = profile.firstName + " " + profile.lastName
-    let email = Meteor.user().emails[0].address
-    let imageUri = "http://placehold.it/350x150"
+    const { currentUser: { emails, profile } } = this.props;
+    const name = `${profile.firstName} ${profile.lastName}`;
+    const email = emails[0].address;
+    const imageUri = "http://placehold.it/350x150"
     return (
       <IconMenu
-          iconButtonElement={<div style={styles.roundButton}>{this.getPlaceholder()}</div>}
-          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-          targetOrigin={{horizontal: 'right', vertical: 'top'}} >
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        iconButtonElement={this.getAvatar(true)}
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}>
         <MenuItem onClick={this.goToUserProfile}>
-          <div style={styles.iconMenuViewProfileContainer}>
-            <div style={styles.iconMenuItemContainer}>
-              <IconButton><Person style={styles.iconStyles}/></IconButton>
-            </div>
-            <div style={styles.iconMenuItemContainer}>
-              <div>{name}</div>
-              <div>{email}</div>
-            </div>
-          </div>
+          <Row>
+            <Col style={styles.iconContainer}>{this.getAvatar(false)}</Col>
+            <Col style={styles.textContainer}>
+              <span style={styles.name}>{name}</span>
+              <span style={styles.email}>{email}</span>
+            </Col>
+          </Row>
         </MenuItem>
-        <Divider/>
-        <MenuItem primaryText="Edit Profile" />
-        <MenuItem primaryText="View Organization Profile - Admin Only?" />
-        <MenuItem primaryText="Edit Organization Profile - Admin Only?" />
+        <Divider />
         <MenuItem onTouchTap={this.goToSettings} primaryText="Change Password" />
         <MenuItem onTouchTap={this.props.signout} primaryText="Sign Out" />
       </IconMenu>
     )
   }
 
-  getPlaceholder() {
-    let userAvatar = 'https://placehold.it/350x150'; //replace this with an actual user avatar url once all that is setup
-    if (userAvatar == undefined) {
-      return getUserInitials();
+  getAvatar(navBar) {
+    const avatar = get(this.props, 'currentUser.profile.avatar.original');
+    if (avatar) {
+      return <img
+        src={original}
+        style={{borderRadius: '50%', height: '48px', width: '48px'}}
+      />;
+    } else {
+      const firstName = get(this.props, 'currentUser.profile.firstName');
+      const lastName = get(this.props, 'currentUser.profile.lastName');
+      const style = (() => {
+        if (navBar) return { ...styles.baseIcon, ...styles.navBarIcon };
+        else return { ...styles.baseIcon, ...styles.menuIcon };
+      })();
+      return <div 
+        style={style}>
+        {`${firstName.charAt(0)}${lastName.charAt(0)}`}
+      </div>;
     }
-    return <img style={{height: '48px', width: '48px', borderRadius: '50%'}} src={userAvatar} />;
-  }
-
-  getUserInitials() {
-    let firstName = Meteor.user().profile.firstName
-    let lastName = Meteor.user().profile.lastName
-    return firstName.charAt(0) + lastName.charAt(0)
   }
 
   goToUserProfile() {
-    browserHistory.push('/users/' + Meteor.userId())
+    const { currentUser: { _id } } = this.props;
+    browserHistory.push(`/users/${_id}`);
   }
 
   goToSettings() {
@@ -82,39 +88,67 @@ class UserProfileMenu extends Component {
 }
 
 const styles = {
+
+  name: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    height: '20px',
+  },
+
+  email: {
+    fontSize: '14px',
+    fontWeight: 'lighter',
+    marginLeft: '8px',
+  },
+
   iconStyles: {
     height: '48px',
     width: '48px',
     margin: '16px 24px',
   },
 
-  iconMenuViewProfileContainer: {
-    display: 'flex',
-    flexDirection: 'row',
+  textContainer: {
+    justifyContent: 'center',
   },
 
-  iconMenuItemContainer: {
+  iconContainer: {
+    justifyContent: 'center',
+  },
+
+  iconMenuItemText: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
   },
 
-  roundButton: {
-    borderRadius: '50%',
-    height: '48px',
-    width: '48px',
-    border: '1px solid white',
-    margin: '16px 24px',
-    backgroundColor: 'white',
-    color: lightBlue800,
-    display: 'flex',
+  baseIcon: {
     alignItems: 'center',
-    justifyContent: 'center',
+    border: '1px solid white',
+    borderRadius: '50%',
+    display: 'flex',
     fontSize: '12px',
-  }
-}
+    height: '48px',
+    justifyContent: 'center',
+    width: '48px',
+  },
 
-const mapStateToProps = () => ({})
+  navBarIcon: {
+    backgroundColor: Colors.white,
+    color: 'black',
+    margin: '16px 24px',
+  },
+
+  menuIcon: {
+    backgroundColor: Colors.primary,
+    color: 'white',
+    marginRight: '12px',
+  },
+
+};
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+})
 
 const mapDispatchToProps = (dispatch) => ({
   signout: () => dispatch(signoutUser())
